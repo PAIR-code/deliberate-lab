fd6912f32cca066fe2a33efabee0a713213fccfa
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-04-05 18:22:15 +0200

    fix auth guard for participants (async behavior)

--- src/app/valid-participant.guard.ts
+++ src/app/valid-participant.guard.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
 import { participantQuery } from 'src/lib/api/queries';
+import { querySuccessPromise } from 'src/lib/utils/queries.utils';
 
 /** Check that a participant exists */
 export const validParticipantGuard: CanActivateFn = (
@@ -11,8 +12,9 @@ export const validParticipantGuard: CanActivateFn = (
   const http = inject(HttpClient);
 
   const { participantId } = route.params;
-
+  // ISSUE: this starts as false ! we must use a promise and wait for this to resolve...
+  // et le pb2 c'est le participant qui veut pas bouger...
   const participant = participantQuery(http, participantId);
 
-  return !!participant.data(); // If the data exists, the participant is valid
+  return querySuccessPromise(participant);
 };
