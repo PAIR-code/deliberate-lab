dec0a83d947bbb792ca554abd7354bb5cc904961
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-04-29 14:19:38 +0200

    replace seeders (DB & Auth) with local scripts

---
+++ scripts/src/admin.ts
@@ -0,0 +1,35 @@
+/** Load the Firebase app and expose it to scripts */
+
+import * as admin from "firebase-admin";
+import * as fs from "fs";
+import credentials from "../service-account.json";
+
+const loadConfig = (fileName: string): any => {
+  try {
+    const rawData = fs.readFileSync(fileName, { encoding: "utf-8" });
+    return JSON.parse(rawData);
+  } catch (error) {
+    console.error(`Error reading or parsing ${fileName}:`, error);
+    throw error;
+  }
+};
+
+export const initializeApp = () => {
+  const firebaserc = loadConfig("../.firebaserc");
+
+  if (process.env.NODE_ENV === "production") {
+    admin.initializeApp({
+      credential: admin.credential.cert(credentials as admin.ServiceAccount),
+    });
+  } else {
+    // Default to local emulators
+    process.env.FIRESTORE_EMULATOR_HOST = "127.0.0.1:8080";
+    process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
+    admin.initializeApp({
+      projectId: firebaserc.projects.default,
+    });
+  }
+  console.log("Firebase app initialized");
+};
+
+export default admin;
