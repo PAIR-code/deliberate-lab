9c163e54d468a6f30f797d25abfdab4bb58994d9
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-05-03 17:33:30 +0200

    update webapp firebase config to make cloud functions url depend on firebase-config.ts

--- webapp/src/environments/environment.development.ts
+++ webapp/src/environments/environment.development.ts
@@ -1,12 +1,8 @@
 /** Development environment configuration */
 
+import { firebaseConfig } from 'src/lib/api/firebase-config';
+
 export const environment = {
   production: false,
-
-  cloudFunctionsUrl: 'http://localhost:5001/friendlychat-d6dc5/us-central1',
-
-  // Google API keys (they have no business being here in the frontend, remove this later)
-  oauthClientId: 'enter you oauth client id here',
-  sheetsApiKey: 'enter api key for sheets api here',
-  driveApiKey: 'enter api key for drive api here',
+  cloudFunctionsUrl: `http://localhost:5001/${firebaseConfig.projectId}/us-central1`,
 };
