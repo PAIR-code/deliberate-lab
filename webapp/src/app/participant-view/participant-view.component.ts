7ae4b955e6b358af45f34b0ede2d6ad28d04be85
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-05-22 11:42:11 +0200

    fix missing route in query string

--- webapp/src/app/participant-view/participant-view.component.ts
+++ webapp/src/app/participant-view/participant-view.component.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an Apache2 license that can be
  * found in the LICENSE file and http://www.apache.org/licenses/LICENSE-2.0
 ==============================================================================*/
-import { Component, ElementRef, ViewChild, computed } from '@angular/core';
+import { Component, ElementRef, ViewChild, computed, effect } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -45,15 +45,33 @@ export class ParticipantViewComponent {
     private router: Router,
   ) {
     // Initialize the participant service
+    const stage = routeQueryStringSignal(route, 'stage');
+
     participantService.initialize(
       routeParamSignal(route, 'experimentId'),
       routeParamSignal(route, 'participantId'),
-      routeQueryStringSignal(route, 'stage'),
+      stage,
     );
 
     this.participantProfile = computed(
       () => participantService.participant()?.profile() ?? undefined,
     );
+
+    // Navigation effect: if the route query parameter signal points to a wrong route or is undefined,
+    // navigate to the correct route
+    effect(() => {
+      const stageName = stage();
+      const stages = participantService.experiment()?.stageNames() ?? [];
+      if (stages.length === 0) return;
+
+      if (stages.includes(stageName!)) return;
+
+      // Navigate to the first stage
+      router.navigate(
+        ['/participant', participantService.experimentId()!, participantService.participantId()!],
+        { queryParams: { stage: stages[0] } },
+      );
+    });
   }
 
   logout() {
