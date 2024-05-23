f1e8672c3d50df88bd8ec2faef15b8d3437312da
Crystal Qian <4844633+cjqian@users.noreply.github.com>
2024-05-23 09:28:17 -0400

    Temporarily host images on Github as a local asset rendering workaround.

--- utils/src/types/items.types.ts
+++ utils/src/types/items.types.ts
@@ -35,85 +35,89 @@ export const ITEM_NAMES = [
   'chocolateBars',
   'fishingKit',
 ] as const;
+
+// Use external hosting as a temporary workaround..
+export const IMAGE_HEADER = 'https://raw.githubusercontent.com/PAIR-code/llm-mediation-experiments/main/webapp/src/assets/items/'
+export const IMAGE_SUFFIX = "?raw=true";
+// export const IMAGE_HEADER = '/assets/items/';
+// export const IMAGE_SUFFIX = "";
+
+
 export type ItemName = (typeof ITEM_NAMES)[number];
 export const ITEMS: Record<ItemName, Item> = {
   sextant: {
     name: 'Compass',
-    imageUrl:
-      '/assets/items/compass.jpeg',
+    imageUrl: IMAGE_HEADER + 'compass.jpeg' + IMAGE_SUFFIX,
     ranking: 15, // Useless without tables and chronometer.
   },
   shavingMirror: {
     name: 'Mirror',
-    imageUrl:
-      '/assets/items/mirror.jpeg',
+    imageUrl: IMAGE_HEADER + 'mirror.jpeg' + IMAGE_SUFFIX,
     ranking: 1,
   },
   mosquitoNetting: {
     name: 'Mosquito netting',
-    imageUrl:
-      '/assets/items/netting.jpeg',
+    imageUrl: IMAGE_HEADER + 'netting.jpeg' + IMAGE_SUFFIX,
     ranking: 14,
   },
   waterContainer: {
     name: 'Water (25L)',
-    imageUrl:
-      '/assets/items/water.jpeg',
+    imageUrl: IMAGE_HEADER + 'water.jpeg' + IMAGE_SUFFIX,
     ranking: 3,
   },
   armyRations: {
     name: 'Case of army rations',
-    imageUrl: '/assets/items/rations.jpeg',
+    imageUrl: IMAGE_HEADER + 'rations.jpeg' + IMAGE_SUFFIX,
     ranking: 4,
   },
   pacificMaps: {
     name: 'Maps of the Atlantic Ocean', 
-    imageUrl: '/assets/items/map.jpeg',
+    imageUrl: IMAGE_HEADER + 'rations.jpeg' + IMAGE_SUFFIX,
     ranking: 13,
   },
   floatingSeatCushion: {
     name: 'Floating seat cushion',
-    imageUrl: '/assets/items/cushion.jpeg',
+    imageUrl: IMAGE_HEADER + 'cushion.jpeg' + IMAGE_SUFFIX,
     ranking: 9,
   },
   canOilMixture: {
     name: 'Can of oil/petrol (10L)',
-    imageUrl: '/assets/items/oil.jpeg',
+    imageUrl: IMAGE_HEADER + 'oil.jpeg' + IMAGE_SUFFIX,
     ranking: 2,
   },
   transistorRadio: {
     name: 'Small transistor radio',
-    imageUrl: '/assets/items/radio.jpeg',
+    imageUrl: IMAGE_HEADER + 'radio.jpeg' + IMAGE_SUFFIX,
     ranking: 12,
   },
   plasticSheeting: {
     name: 'Plastic sheeting',
-    imageUrl: '/assets/items/sheeting.jpeg',
+    imageUrl: IMAGE_HEADER + 'sheeting.jpeg' + IMAGE_SUFFIX,
     ranking: 5, 
   },
   sharkRepellent: {
     name: 'Can of shark repellent',
-    imageUrl: '/assets/items/repellent.jpeg',
+    imageUrl: IMAGE_HEADER + 'repellent.jpeg' + IMAGE_SUFFIX,
     ranking: 10,
   },
   rubbingAlcohol: {
     name: 'Bottle of rubbing alcohol',
-    imageUrl: '/assets/items/rubbing_alcohol.jpeg',
+    imageUrl: IMAGE_HEADER + 'rubbing_alcohol.jpeg' + IMAGE_SUFFIX,
     ranking: 11,
   },
   nylonRope: {
     name: 'Nylon rope (15 ft.)',
-    imageUrl: '/assets/items/rope.jpeg',
+    imageUrl: IMAGE_HEADER + 'rope.jpeg' + IMAGE_SUFFIX,
     ranking: 8,
   },
   chocolateBars: {
     name: 'Chocolate bars (2 boxes)',
-    imageUrl: '/assets/items/chocolate.jpeg',
+    imageUrl: IMAGE_HEADER + 'chocolate.jpeg' + IMAGE_SUFFIX,
     ranking: 6,
   },
   fishingKit: {
     name: 'A fishing kit & pole',
-    imageUrl: '/assets/items/fishing.jpeg',
+    imageUrl: IMAGE_HEADER + 'fishing.jpeg' + IMAGE_SUFFIX,
     ranking: 7,
   },
 };
