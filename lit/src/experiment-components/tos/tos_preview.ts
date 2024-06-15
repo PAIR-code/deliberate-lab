fe58fcedb6c279c3acb689f93c48cf144a60d627
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2024-06-14 19:22:46 -0700

    Enable participants to update Terms of Service stage

--- lit/src/experiment-components/tos/tos_preview.ts
+++ lit/src/experiment-components/tos/tos_preview.ts
@@ -1,5 +1,6 @@
 import "../../pair-components/icon_button";
 
+import "@material/web/checkbox/checkbox.js";
 import * as sanitizeHtml from "sanitize-html";
 
 import { observable } from "mobx";
@@ -9,6 +10,10 @@ import { customElement, property } from "lit/decorators.js";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 
 import { TermsOfServiceStageConfig } from "@llm-mediation-experiments/utils";
+import { Timestamp } from "firebase/firestore";
+
+import { core } from "../../core/core";
+import { ParticipantService } from "../../services/participant_service";
 
 import { styles } from "./tos_preview.scss";
 
@@ -17,27 +22,42 @@ import { styles } from "./tos_preview.scss";
 export class TOSPreview extends MobxLitElement {
   static override styles: CSSResultGroup = [styles];
 
+  private readonly participantService = core.getService(ParticipantService);
+
   @property() stage: TermsOfServiceStageConfig|null = null;
 
   override render() {
-    if (!this.stage) {
+    if (!this.stage || this.participantService.profile === undefined) {
       return nothing;
     }
 
     const cleanHTML = sanitizeHtml(this.stage?.tosLines.join('\n\n'));
+
+    const timestamp = this.participantService.profile?.acceptTosTimestamp;
+    const handleTOSClick = () => {
+      const acceptTosTimestamp = timestamp ? null : Timestamp.now();
+      this.participantService.updateProfile({ acceptTosTimestamp });
+    };
+
     return html`
       <div class="tos-wrapper">
         ${unsafeHTML(cleanHTML)}
       </div>
       <div class="ack-wrapper">
-        <pr-icon-button
-          color="neutral"
-          icon="check_box_outline_blank"
-          variant="default"
-          disabled
-        >
-        </pr-icon-button>
-        <div>I acknowledge the Terms of Service</div>
+        <label class="checkbox-wrapper">
+          <md-checkbox
+            touch-target="wrapper"
+            aria-label="Accept the Terms of Service"
+            ?checked=${timestamp !== null}
+            @click=${handleTOSClick}
+          >
+          </md-checkbox>
+          I accept the Terms of Service
+        </label>
+        <div class="timestamp-wrapper">
+          ${timestamp ?
+            `Accepted at ${new Date(timestamp.seconds * 1000)}` : nothing}
+        </div>
       </div>
     `;
   }
