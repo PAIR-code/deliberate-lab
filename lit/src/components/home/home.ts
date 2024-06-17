017c1410a9827242b6fccd635b3ac0a346fabbb3
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2024-06-17 12:22:27 -0700

    Update Home page to show links to experiments, templates
    This also moves the "Create experiment" button to the Home page header.

--- lit/src/components/home/home.ts
+++ lit/src/components/home/home.ts
@@ -6,12 +6,13 @@ import { MobxLitElement } from "@adobe/lit-mobx";
 import { CSSResultGroup, html, nothing } from "lit";
 import { customElement, state } from "lit/decorators.js";
 
-import { Experiment } from '@llm-mediation-experiments/utils';
+import { Experiment, ExperimentTemplate } from '@llm-mediation-experiments/utils';
 
 import { core } from "../../core/core";
 import { AuthService } from "../../services/auth_service";
 import { FirebaseService } from "../../services/firebase_service";
 import { Pages, RouterService } from "../../services/router_service";
+import { ExperimentConfigService } from "../../services/config/experiment_config_service";
 
 import { StageKind } from "@llm-mediation-experiments/utils";
 
@@ -26,6 +27,7 @@ export class Home extends MobxLitElement {
   private readonly authService = core.getService(AuthService);
   private readonly routerService = core.getService(RouterService);
   private readonly experimenterService = core.getService(ExperimenterService);
+  private readonly experimentConfig = core.getService(ExperimentConfigService);
 
   override render() {
     if (!this.authService.isExperimenter) {
@@ -33,39 +35,78 @@ export class Home extends MobxLitElement {
     }
 
     return html`
-      ${this.renderCreateButton()}
+      <h2>Experiments</h2>
       <div class="cards-wrapper">
+        ${this.experimenterService.experiments.length === 0 ?
+          html`<div class="label">No experiments yet</div>` : nothing}
         ${this.experimenterService.experiments.map(
           experiment => this.renderExperimentCard(experiment)
+        )}
+      </div>
+      <h2>Templates</h2>
+      <div class="cards-wrapper">
+        ${this.experimenterService.templates.length === 0 ?
+          html`<div class="label">No templates yet</div>` : nothing}
+        ${this.experimenterService.templates.map(
+          template => this.renderTemplateCard(template)
         )}
       </div>
     `;
   }
 
-  private renderCreateButton() {
-    if (!this.authService.isExperimenter) {
-      return nothing;
-    }
-
-    const handleClick = () => {
-      this.routerService.navigate(Pages.EXPERIMENT_CREATE);
-    }
-
-    return html`
-      <pr-button @click=${handleClick}>Create new experiment</pr-button>
-    `;
-  }
   private renderExperimentCard(experiment: Experiment) {
+    const handleClick = () => {
+      this.routerService.navigate(
+        Pages.EXPERIMENT,
+        { "experiment": experiment.id }
+      );
+    }
+
     const handleDelete = () => {
       this.experimenterService.deleteExperiment(experiment.id);
     };
 
     return html`
       <div class="card">
-        <h2>${experiment.name}</h2>
-        <p>Id: ${experiment.id}</p>
+        <h3>${experiment.name}</h3>
+        <p class="label">${experiment.numberOfParticipants} participants</p>
+        <p class="label">ID: ${experiment.id}</p>
         <div class="action-buttons">
+          <pr-button variant="default" @click=${handleClick}>
+            View experiment
+          </pr-button>
           <pr-tooltip text="Delete experiment" position="BOTTOM_END">
+            <pr-icon-button
+              icon="delete"
+              color="error"
+              variant="default"
+              @click=${handleDelete}>
+            </pr-icon-button>
+          </pr-tooltip>
+        </div>
+      </div>
+    `;
+  }
+
+  private renderTemplateCard(template: ExperimentTemplate) {
+    const handleClick = () => {
+      this.experimentConfig.loadTemplate(template.id, template.name);
+      this.routerService.navigate(Pages.EXPERIMENT_CREATE);
+    }
+
+    const handleDelete = () => {
+      this.experimenterService.deleteTemplate(template.id);
+    };
+
+    return html`
+      <div class="card">
+        <h3>${template.name}</h3>
+        <p class="label">ID: ${template.id}</p>
+        <div class="action-buttons">
+          <pr-button variant="default" @click=${handleClick}>
+            Use template
+          </pr-button>
+          <pr-tooltip text="Delete template" position="BOTTOM_END">
             <pr-icon-button
               icon="delete"
               color="error"
