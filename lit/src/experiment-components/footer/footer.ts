e49a52ad6fe591438e10b3d949756f69505a83e1
Crystal Qian <4844633+cjqian@users.noreply.github.com>
2024-07-29 16:40:04 -0400

    Add a 5-minute countdown timer to the lobby.

--- lit/src/experiment-components/footer/footer.ts
+++ lit/src/experiment-components/footer/footer.ts
@@ -1,14 +1,13 @@
 import "../../pair-components/button";
 
 import { MobxLitElement } from "@adobe/lit-mobx";
-import { CSSResultGroup, html, nothing } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { CSSResultGroup, html } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 
 import { core } from "../../core/core";
 import { ExperimentService } from "../../services/experiment_service";
 import { ParticipantService } from "../../services/participant_service";
 import { Pages, RouterService } from "../../services/router_service";
-import { SurveyService } from "../../services/survey_service";
 
 import { styles } from "./footer.scss";
 
@@ -20,9 +19,50 @@ export class Footer extends MobxLitElement {
   private readonly experimentService = core.getService(ExperimentService);
   private readonly participantService = core.getService(ParticipantService);
   private readonly routerService = core.getService(RouterService);
-  private readonly surveyService = core.getService(SurveyService);
 
-  @property() disabled = false;
+  @property() disabled = true; // Initially disabled
+  @state() private timeRemaining = 5 * 60; // Set initial countdown time to 5 minutes (300 seconds)
+
+  private countdownInterval: number | undefined;
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.experimentService.experiment?.isLobby) {
+      this.startCountdown();
+    } else {
+      this.disabled = false; // Enable the button if it's not a lobby experiment
+    }
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.clearCountdown();
+  }
+
+  startCountdown() {
+    this.clearCountdown(); // Ensure no previous intervals are running
+    this.countdownInterval = window.setInterval(() => {
+      if (this.timeRemaining > 0) {
+        this.timeRemaining -= 1;
+      } else {
+        this.disabled = false; // Enable the button when countdown reaches zero
+        this.clearCountdown();
+      }
+    }, 1000);
+  }
+
+  clearCountdown() {
+    if (this.countdownInterval !== undefined) {
+      window.clearInterval(this.countdownInterval);
+      this.countdownInterval = undefined;
+    }
+  }
+
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes}:${secs.toString().padStart(2, '0')}`;
+  }
 
   override render() {
     return html`
@@ -44,12 +84,7 @@ export class Footer extends MobxLitElement {
 
     const isLastStage = index === this.experimentService.stageIds.length - 1;
 
-    const handleNext = async () => {
-      // If survey stage, save relevant text answers
-      if (this.surveyService.stageId === this.participantService.profile?.currentStageId) {
-        await this.surveyService.saveTextAnswers();
-      }
-
+    const handleNext = () => {
       const nextStageId = this.experimentService.getNextStageId(
         this.participantService.profile?.currentStageId ?? ""
       );
@@ -111,7 +146,16 @@ export class Footer extends MobxLitElement {
           </pr-button>
         `;
       } else {
-        return nothing;
+          return html`
+            <pr-button
+              variant=${this.disabled ? "default" : "tonal"}
+              ?disabled=${preventNextClick}
+              @click=${handleNext}
+            >
+              ${isLastStage ? "End experiment" : "Next stage"} 
+              ${this.experimentService.experiment?.isLobby && this.disabled ? ` (${this.formatTime(this.timeRemaining)})` : ""}
+            </pr-button>
+          `;
       }
     }
 
