9a54eff3041c67d361f6721bbcbe8515df1d389b
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2024-09-06 06:04:42 -0700

    Remove obsolete utils, functions

--- utils/src/validation/lost_at_sea.validation.ts
+++
@@ -1,5 +0,0 @@
-import { Type, type Static } from '@sinclair/typebox';
-import { ITEM_NAMES } from '../types/lost_at_sea.types';
-
-export const ItemData = Type.Union(ITEM_NAMES.map((item) => Type.Literal(item)));
-export type ItemData = Static<typeof ItemData>;
