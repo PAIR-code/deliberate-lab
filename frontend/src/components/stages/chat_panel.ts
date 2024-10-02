2e5f907b540895cf2c31ceabcc09db56f82e0d3d
Crystal Qian <4844633+cjqian@users.noreply.github.com>
2024-10-02 15:47:45 -0400

    Render a list of agents in chat view for experimenters.

--- frontend/src/components/stages/chat_panel.ts
+++ frontend/src/components/stages/chat_panel.ts
@@ -1,3 +1,4 @@
+import '../../pair-components/tooltip';
 import './stage_description';
 import './stage_footer';
 
@@ -6,15 +7,15 @@ import {CSSResultGroup, html, nothing} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 import {core} from '../../core/core';
+import {AuthService} from '../../services/auth.service';
 import {CohortService} from '../../services/cohort.service';
 
 import {
   ChatStageConfig,
+  MediatorConfig,
   ParticipantProfile,
 } from '@deliberation-lab/utils';
-import {
-  isActiveParticipant
-} from '../../shared/participant.utils';
+import {isActiveParticipant} from '../../shared/participant.utils';
 
 import {styles} from './chat_panel.scss';
 
@@ -23,6 +24,7 @@ import {styles} from './chat_panel.scss';
 export class ChatPanel extends MobxLitElement {
   static override styles: CSSResultGroup = [styles];
 
+  private readonly authService = core.getService(AuthService);
   private readonly cohortService = core.getService(CohortService);
 
   @property() stage: ChatStageConfig | null = null;
@@ -34,7 +36,25 @@ export class ChatPanel extends MobxLitElement {
 
     return html`
       <stage-description .stage=${this.stage}></stage-description>
-      ${this.renderParticipantList()}
+      ${this.renderParticipantList()} ${this.renderMediatorsList()}
+    `;
+  }
+
+  private renderMediatorsList() {
+    if (!this.authService.isExperimenter || !this.stage) {
+      return;
+    }
+
+    const mediators = this.stage.mediators;
+    if (!mediators) {
+      return;
+    }
+    return html`
+      <div class="panel-item">
+        <div class="panel-item-title">Agents (${mediators.length})</div>
+        <div class="panel-item-subtitle">Only visible to experimenters</div>
+        ${mediators.map((mediator) => this.renderMediator(mediator))}
+      </div>
     `;
   }
 
@@ -45,13 +65,23 @@ export class ChatPanel extends MobxLitElement {
         <div class="panel-item-title">
           Participants (${activeParticipants.length})
         </div>
-        ${activeParticipants.map(
-          participant => this.renderProfile(participant)
+        ${activeParticipants.map((participant) =>
+          this.renderProfile(participant)
         )}
       </div>
     `;
   }
 
+  private renderMediator(mediator: MediatorConfig) {
+    return html`
+      <pr-tooltip text=${mediator.prompt} position="BOTTOM_END">
+        <div class="profile">
+          <profile-avatar .emoji=${mediator.avatar}> </profile-avatar>
+          <div class="name">${mediator.name}</div>
+        </div>
+      </pr-tooltip>
+    `;
+  }
   private renderProfile(profile: ParticipantProfile) {
     return html`
       <div class="profile">
