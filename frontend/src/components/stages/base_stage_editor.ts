1d8291aabe472e0a1d71c1958c02f1da1545e6e2
Crystal Qian <4844633+cjqian@users.noreply.github.com>
2024-10-02 15:00:35 -0400

    Force reveal stage to 'wait for all participants' if there's a dependency on participant data.

--- frontend/src/components/stages/base_stage_editor.ts
+++ frontend/src/components/stages/base_stage_editor.ts
@@ -10,6 +10,9 @@ import {core} from '../../core/core';
 import {ExperimentEditor} from '../../services/experiment.editor';
 
 import {
+  ElectionStrategy,
+  RevealAudience,
+  RankingType,
   StageConfig,
   StageKind,
   StageProgressConfig,
@@ -24,7 +27,7 @@ export class BaseStageEditorComponent extends MobxLitElement {
 
   private readonly experimentEditor = core.getService(ExperimentEditor);
 
-  @property() stage: StageConfig|undefined = undefined;
+  @property() stage: StageConfig | undefined = undefined;
 
   override render() {
     if (this.stage === undefined) {
@@ -32,21 +35,18 @@ export class BaseStageEditorComponent extends MobxLitElement {
     }
 
     return html`
-      ${this.renderName()}
-      ${this.renderPrimaryText()}
-      ${this.renderInfoText()}
-      ${this.renderHelpText()}
-      ${this.renderMinParticipants()}
+      ${this.renderName()} ${this.renderPrimaryText()} ${this.renderInfoText()}
+      ${this.renderHelpText()} ${this.renderMinParticipants()}
       ${this.renderWaitForAllParticipants()}
       ${this.renderShowParticipantProgress()}
     `;
   }
 
   private renderName() {
-    const updateName= (e: InputEvent) => {
+    const updateName = (e: InputEvent) => {
       const name = (e.target as HTMLTextAreaElement).value;
       if (this.stage) {
-        this.experimentEditor.updateStage({ ...this.stage, name });
+        this.experimentEditor.updateStage({...this.stage, name});
       }
     };
 
@@ -67,8 +67,8 @@ export class BaseStageEditorComponent extends MobxLitElement {
     const update = (e: InputEvent) => {
       const primaryText = (e.target as HTMLTextAreaElement).value;
       if (this.stage) {
-        const descriptions = { ...this.stage.descriptions, primaryText };
-        this.experimentEditor.updateStage({ ...this.stage, descriptions });
+        const descriptions = {...this.stage.descriptions, primaryText};
+        this.experimentEditor.updateStage({...this.stage, descriptions});
       }
     };
 
@@ -89,8 +89,8 @@ export class BaseStageEditorComponent extends MobxLitElement {
     const update = (e: InputEvent) => {
       const infoText = (e.target as HTMLTextAreaElement).value;
       if (this.stage) {
-        const descriptions = { ...this.stage.descriptions, infoText };
-        this.experimentEditor.updateStage({ ...this.stage, descriptions });
+        const descriptions = {...this.stage.descriptions, infoText};
+        this.experimentEditor.updateStage({...this.stage, descriptions});
       }
     };
 
@@ -111,8 +111,8 @@ export class BaseStageEditorComponent extends MobxLitElement {
     const update = (e: InputEvent) => {
       const helpText = (e.target as HTMLTextAreaElement).value;
       if (this.stage) {
-        const descriptions = { ...this.stage.descriptions, helpText };
-        this.experimentEditor.updateStage({ ...this.stage, descriptions });
+        const descriptions = {...this.stage.descriptions, helpText};
+        this.experimentEditor.updateStage({...this.stage, descriptions});
       }
     };
 
@@ -129,15 +129,89 @@ export class BaseStageEditorComponent extends MobxLitElement {
     `;
   }
 
+  private shouldDisableWaitForAllParticipants(): boolean {
+    if (!this.stage || this.stage.kind !== StageKind.REVEAL) {
+      return false;
+    }
+
+    for (const item of this.stage.items) {
+      // There's a dependency on all participants if we want to reveal all results.
+      if (
+        'revealAudience' in item &&
+        item.revealAudience === RevealAudience.ALL_PARTICIPANTS
+      ) {
+        return true;
+      }
+      if (item.kind === StageKind.RANKING) {
+        const stageId = item.id;
+
+        const foundStage = this.experimentEditor.stages.find(
+          (stage) => stage.id === stageId
+        );
+        // There's a dependency on all participants if we're ranking all participants.
+        if (
+          foundStage &&
+          'rankingType' in foundStage &&
+          foundStage.rankingType === RankingType.PARTICIPANTS
+        ) {
+          return true;
+        }
+
+        // There's a dependency on all participants if there's an election
+        // (so all votes are counted).
+        if (
+          foundStage &&
+          'strategy' in foundStage &&
+          foundStage.strategy === ElectionStrategy.CONDORCET
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
   private renderWaitForAllParticipants() {
     if (!this.stage) return nothing;
     const waitForAllParticipants = this.stage.progress.waitForAllParticipants;
 
     const updateCheck = () => {
       if (!this.stage) return;
-      const progress: StageProgressConfig = { ...this.stage.progress, waitForAllParticipants: !waitForAllParticipants };
-      this.experimentEditor.updateStage({ ...this.stage, progress });
+      const progress: StageProgressConfig = {
+        ...this.stage.progress,
+        waitForAllParticipants: !waitForAllParticipants,
+      };
+      this.experimentEditor.updateStage({...this.stage, progress});
     };
+
+    if (this.shouldDisableWaitForAllParticipants()) {
+      if (!waitForAllParticipants) {
+        const progress: StageProgressConfig = {
+          ...this.stage.progress,
+          waitForAllParticipants: true,
+        };
+        this.experimentEditor.updateStage({...this.stage, progress});
+      }
+
+      return html`
+        <div class="config-item">
+          <div class="checkbox-wrapper">
+            <md-checkbox
+              touch-target="wrapper"
+              ?checked="${true}"
+              ?disabled="${true}"
+            >
+            </md-checkbox>
+            <div>Wait for all participants before starting stage<br/</div>
+            <div class="warning">
+              Because this experiment has a dependency on all participants'
+              responses, this must be enabled.
+            </div>
+          </div>
+        </div>
+      `;
+    }
 
     return html`
       <div class="config-item">
@@ -149,9 +223,7 @@ export class BaseStageEditorComponent extends MobxLitElement {
             @click=${updateCheck}
           >
           </md-checkbox>
-          <div>
-            Wait for all participants before starting stage
-          </div>
+          <div>Wait for all participants before starting stage</div>
         </div>
       </div>
     `;
@@ -163,8 +235,11 @@ export class BaseStageEditorComponent extends MobxLitElement {
 
     const updateCheck = () => {
       if (!this.stage) return;
-      const progress: StageProgressConfig = { ...this.stage.progress, showParticipantProgress: !showParticipantProgress };
-      this.experimentEditor.updateStage({ ...this.stage, progress });
+      const progress: StageProgressConfig = {
+        ...this.stage.progress,
+        showParticipantProgress: !showParticipantProgress,
+      };
+      this.experimentEditor.updateStage({...this.stage, progress});
     };
 
     return html`
@@ -178,7 +253,8 @@ export class BaseStageEditorComponent extends MobxLitElement {
           >
           </md-checkbox>
           <div>
-            Show participant progress (number of participants who have completed stage)
+            Show participant progress (number of participants who have completed
+            stage)
           </div>
         </div>
       </div>
@@ -191,16 +267,17 @@ export class BaseStageEditorComponent extends MobxLitElement {
     const updateNum = (e: InputEvent) => {
       if (!this.stage) return;
       const minParticipants = Number((e.target as HTMLTextAreaElement).value);
-      const progress: StageProgressConfig = { ...this.stage.progress, minParticipants };
-      this.experimentEditor.updateStage({ ...this.stage, progress });
+      const progress: StageProgressConfig = {
+        ...this.stage.progress,
+        minParticipants,
+      };
+      this.experimentEditor.updateStage({...this.stage, progress});
     };
 
     return html`
       <div class="config-item">
         <div class="number-input">
-          <label for="minParticipants">
-            Minimum number of participants
-          </label>
+          <label for="minParticipants"> Minimum number of participants </label>
           <input
             type="number"
             id="minParticipants"
