b81bf5581c15b62b4ed5111938c42ad5384afc5c
Crystal Qian <4844633+cjqian@users.noreply.github.com>
2024-10-03 13:20:08 -0400

    Randomly sample an agent message to send out of multiple agents.

--- functions/src/stages/chat.triggers.ts
+++ functions/src/stages/chat.triggers.ts
@@ -5,11 +5,18 @@ import {
   ChatMessageType,
   StageKind,
   addChatHistoryToPrompt,
-  createAgentMediatorChatMessage
+  createAgentMediatorChatMessage,
+  MediatorConfig,
 } from '@deliberation-lab/utils';
 
 import { app } from '../app';
 import { getGeminiAPIResponse } from '../api/gemini.api';
+
+export interface MediatorMessage {
+  mediator: MediatorConfig;
+  parsed: any;
+  message: string;
+}
 
 /** When chat message is created, generate mediator response if relevant. */
 export const createMediatorMessage = onDocumentCreated(
@@ -20,75 +27,97 @@ export const createMediatorMessage = onDocumentCreated(
 
     // Use experiment config to get ChatStageConfig with mediators.
     const stage = (
-      await app.firestore().doc(`experiments/${event.params.experimentId}/stages/${event.params.stageId}`).get()
+      await app
+        .firestore()
+        .doc(`experiments/${event.params.experimentId}/stages/${event.params.stageId}`)
+        .get()
     ).data() as StageConfig;
-    if (stage.kind !== StageKind.CHAT) { return; }
+    if (stage.kind !== StageKind.CHAT) {
+      return;
+    }
 
     // Fetch experiment creator's API key.
-    const creatorId = (await app.firestore().collection('experiments').doc(event.params.experimentId).get())
-      .data().metadata.creator;
-    const creatorDoc = (await app.firestore().collection('experimenterData').doc(creatorId).get());
+    const creatorId = (
+      await app.firestore().collection('experiments').doc(event.params.experimentId).get()
+    ).data().metadata.creator;
+    const creatorDoc = await app.firestore().collection('experimenterData').doc(creatorId).get();
     if (!creatorDoc.exists) return;
 
     const apiKeys = creatorDoc.data().apiKeys;
 
-    for (const mediator of stage.mediators) {
-      // Use chats in collection to build chat history for prompt, get num chats
-      const chatMessages = (
-        await app
+    // Use chats in collection to build chat history for prompt, get num chats
+    const chatMessages = (
+      await app
         .firestore()
-        .collection(`experiments/${event.params.experimentId}/cohorts/${event.params.cohortId}/publicStageData/${event.params.stageId}/chats`)
+        .collection(
+          `experiments/${event.params.experimentId}/cohorts/${event.params.cohortId}/publicStageData/${event.params.stageId}/chats`,
+        )
         .orderBy('timestamp', 'asc')
-        .get())
-      .docs.map(doc => doc.data() as ChatMessage);
+        .get()
+    ).docs.map((doc) => doc.data() as ChatMessage);
 
+    // Fetch messages from all mediators
+    const mediatorMessages: MediatorMessage[] = [];
+    for (const mediator of stage.mediators) {
       // Use last 10 messages to build chat history
       const prompt = addChatHistoryToPrompt(chatMessages.slice(-10), mediator.prompt);
-      const numChatsBeforeMediator = chatMessages.length;
 
       // Call Gemini API with given modelCall info
       const response = await getGeminiAPIResponse(apiKeys.geminiKey, prompt);
 
-      // If number of chats has not changed, add mediator message
-      const numChatsAfterMediator = (
-        await app
-        .firestore()
-        .collection(`experiments/${event.params.experimentId}/cohorts/${event.params.cohortId}/publicStageData/${event.params.stageId}/chats`)
-        .count().get())
-      .data().count;
-      if (numChatsAfterMediator > numChatsBeforeMediator) { break; }
-
       // Add mediator message if non-empty
       const parsed = JSON.parse(response.text);
       const isJSON = mediator.responseConfig.isJSON;
-      const message = isJSON ?
-        (parsed[mediator.responseConfig.messageField] ?? '') : response.text;
+      const message = isJSON ? (parsed[mediator.responseConfig.messageField] ?? '') : response.text;
 
       if (message.trim() === '') break;
+      mediatorMessages.push({ mediator, parsed, message });
+    }
 
-      const mediatorMessage = createAgentMediatorChatMessage(
-        {
-          profile: { name: mediator.name, avatar: mediator.avatar, pronouns: null },
-          discussionId: data.discussionId,
-          message,
-          timestamp: Timestamp.now(),
-          mediatorId: mediator.id,
-          explanation: isJSON ? (parsed[mediator.responseConfig.explanationField] ?? '') : '',
-        }
-      );
-      const mediatorDocument = app.firestore()
-        .collection('experiments')
-        .doc(event.params.experimentId)
-        .collection('cohorts')
-        .doc(event.params.cohortId)
-        .collection('publicStageData')
-        .doc(event.params.stageId)
-        .collection('chats')
-        .doc(mediatorMessage.id);
+    if (mediatorMessages.length === 0) return;
 
-      await app.firestore().runTransaction(async (transaction) => {
-        transaction.set(mediatorDocument, mediatorMessage);
-      });
-    } // end mediator loop
-  }
+    // Randomly sample a message.
+    const mediatorMessage = mediatorMessages[Math.floor(Math.random() * mediatorMessages.length)];
+    const mediator = mediatorMessage.mediator;
+    const message = mediatorMessage.message;
+    const parsed = mediatorMessage.parsed;
+
+    // Don't send a message if the conversation has moved on. 
+    const numChatsBeforeMediator = chatMessages.length;
+    const numChatsAfterMediator = (
+      await app
+        .firestore()
+        .collection(
+          `experiments/${event.params.experimentId}/cohorts/${event.params.cohortId}/publicStageData/${event.params.stageId}/chats`,
+        )
+        .count()
+        .get()
+    ).data().count;
+    if (numChatsAfterMediator > numChatsBeforeMediator) {
+      return;
+    }
+
+    const chatMessage = createAgentMediatorChatMessage({
+      profile: { name: mediator.name, avatar: mediator.avatar, pronouns: null },
+      discussionId: data.discussionId,
+      message,
+      timestamp: Timestamp.now(),
+      mediatorId: mediator.id,
+      explanation: mediator.responseConfig.isJSON ? (parsed[mediator.responseConfig.explanationField] ?? '') : '',
+    });
+    const mediatorDocument = app
+      .firestore()
+      .collection('experiments')
+      .doc(event.params.experimentId)
+      .collection('cohorts')
+      .doc(event.params.cohortId)
+      .collection('publicStageData')
+      .doc(event.params.stageId)
+      .collection('chats')
+      .doc(chatMessage.id);
+
+    await app.firestore().runTransaction(async (transaction) => {
+      transaction.set(mediatorDocument, chatMessage);
+    });
+  },
 );
