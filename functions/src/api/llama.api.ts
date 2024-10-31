1f924c21f6608a17324ba80a40f63848b0f2d041
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-10-31 16:42:55 +0200

    conform with project style

--- functions/src/api/llama.api.ts
+++ functions/src/api/llama.api.ts
@@ -17,39 +17,39 @@ type OutgoingMessage = {
 }
 
 
-async function chat(llm_server_endpoint_url: string, message: string, model_type: string): Promise<string> {
-    const message_object = encode_message(model_type, message)
-    const response = await fetch(llm_server_endpoint_url, { method: "POST", body: JSON.stringify(message_object) })
-    const response_message = decode_model_response(response)
-    return response_message
+export async function llamaChat(llm_server_endpoint_url: string, message: string, model_type: string): Promise<string> {
+    const message_object = encodeMessage(model_type, message);
+    const response = await fetch(llm_server_endpoint_url, { method: "POST", body: JSON.stringify(message_object) });
+    const response_message = decodeResponse(response);
+    return response_message;
 }
 
 
-function encode_message(model_type: string, message: string): OutgoingMessage {
+function encodeMessage(model_type: string, message: string): OutgoingMessage {
     return { "model": model_type, "prompt": message }
 }
 
 
-async function decode_model_response(response: Response): Promise<string> {
-    const reader = response.body?.getReader()
+async function decodeResponse(response: Response): Promise<string> {
+    const reader = response.body?.getReader();
     if (!reader) {
-        throw new Error("Failed to read response body")
+        throw new Error("Failed to read response body");
     }
 
     let content = ""
     while(true) {
         // we rely on the "done" property given to us in the response JSON
         // to terminate the loop
-        const { done, value } = await reader.read()
+        const { done, value } = await reader.read();
 
         const rawjson = new TextDecoder().decode(value);
-        const json: IncomingMessage = JSON.parse(rawjson)
+        const json: IncomingMessage = JSON.parse(rawjson);
 
         if (json.done) {
-            break
+            break;
         }
-        content += json.response
+        content += json.response;
     } 
 
-    return content
+    return content;
 }
