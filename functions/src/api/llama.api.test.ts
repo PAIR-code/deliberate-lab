4ec06396213c92116b5300e423a1af33ab1ec90a
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-10-31 16:27:29 +0200

    include configurations for tests
    we can remove the tests later but they are essential for now

---
+++ functions/src/api/llama.api.test.ts
@@ -0,0 +1,9 @@
+/**
+ * Test assumes a container with ollama is running on port 11434
+ * Download the docker image to run :
+ * https://ollama.com/blog/ollama-is-now-available-as-an-official-docker-image
+ */
+
+const MODEL = "llama3.2"
+const LLM_SERVER_ENDPOINT = "http://localhost:11434/api/chat"
+
