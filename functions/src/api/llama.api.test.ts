77c3a4fbe007a8dae79b412ff7eaa704365721ed
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-11-01 12:34:02 +0200

    successfully connect class to server and support multiple messages

--- functions/src/api/llama.api.test.ts
+++ functions/src/api/llama.api.test.ts
@@ -6,14 +6,14 @@ import { OllamaChat } from "./llama.api";
  * https://ollama.com/blog/ollama-is-now-available-as-an-official-docker-image
  */
 
-const MODEL_TYPE = "llama3.2"
-const LLM_SERVER_ENDPOINT = "http://localhost:11434/api/chat"
-const TEST_MESSAGE = "Say the following phrase: 'The test succedeed'";
+const MODEL_TYPE = "llama3.2";
+const LLM_SERVER_ENDPOINT = "http://localhost:11434/api/chat";
+const TEST_MESSAGE = "Say hi!'";
 
 
 test("chat with hosted llm", async () => {
     const client = new OllamaChat(LLM_SERVER_ENDPOINT, MODEL_TYPE)
-    const response = await client.chat(TEST_MESSAGE);
-    expect(response).toContain("test");
+    const response = await client.chat([TEST_MESSAGE]);
+    expect(response).toContain("hi");
     console.log(response);
 });
--- functions/src/api/llama.api.ts
+++ functions/src/api/llama.api.ts
@@ -2,19 +2,27 @@
  * Code assumes that the LLaMa instance is hosted on the provided IP address,
  * and is managed through the `ollama` framework (https://github.com/ollama/ollama).
  * Ideally, the code should accomodate multiple frameworks in the end.
+ * 
+ * Note: there already exists a client library for JavaScript, but not for Typescript.
  */
 
 type IncomingMessage = {
     model: string,
     created_at: Date,
-    response: string,
+    message: LlmMessage,
+    done_reason: string,
     done: boolean
 }
 
 type OutgoingMessage = {
     model: string,
-    prompt: string,
+    messages: LlmMessage[],
     stream: boolean
+}
+
+type LlmMessage = {
+    role: string,
+    content: string
 }
 
 
@@ -29,10 +37,24 @@ export class OllamaChat {
         this.isInitalized = false;
     }
 
-    public async chat(message: string): Promise<string> {
-        const message_object = this.encodeMessage(message);
-        const response = await fetch(this.server_endpoint_url, { method: "POST", body: JSON.stringify(message_object) });
-        const response_message = this.decodeResponse(response);
+    public async chat(messages: string[]): Promise<string> {
+        if (!this.isInitalized) {
+            await this.initializeLLM();
+            this.isInitalized = true;
+        }
+        return await this.sendMessage(messages);
+    }
+
+    private initializeLLM(): void {
+        this.sendMessage([])
+    }
+
+    private async sendMessage(messages: string[]): Promise<string> {
+        const message_objects = this.encodeMessages(messages);
+        console.log("Sending prompt:", message_objects)
+        const response = await fetch(this.server_endpoint_url, { method: "POST", body: JSON.stringify(message_objects) });
+        const response_message = await this.decodeResponse(response);
+        console.log("Received response: ", response_message);
         return response_message;
     }
 
@@ -44,19 +66,23 @@ export class OllamaChat {
 
         const { done, value } = await reader.read();
         const rawjson = new TextDecoder().decode(value);
-        console.log("Received: ", rawjson)
+        console.log("Received raw: ", rawjson)
         if (this.isError(rawjson)) {
-            console.log(rawjson)
+            console.log("Error:", rawjson)
             return ""
         } else {
             const json: IncomingMessage = JSON.parse(rawjson);
-            console.log("Received: ", json.response)
-            return json.response;
+            return json.message.content;
         }
     }
 
-    private encodeMessage(message: string): OutgoingMessage {
-        return { "model": this.model_type, "prompt": message, "stream": false }
+    private encodeMessages(messages: string[]): OutgoingMessage {
+        const message_objs: LlmMessage[] = messages.map((message) => ({ role: "user", content: message }));
+        return {
+            model: this.model_type,
+            messages: message_objs,
+            stream: false
+        };
     }
 
     private isError(rawjson: string): boolean {
