648f05a3843a294276c49db04fb9024fd1974a19
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-11-19 13:25:20 +0200

    revert ollama model pull script

--- llm_server/run_ollama.sh
+++ llm_server/run_ollama.sh
@@ -4,6 +4,14 @@
 # in the future these will be incorporated to a docker file
 # https://stackoverflow.com/questions/78232178/ollama-in-docker-pulls-models-via-interactive-shell-but-not-via-run-command-in-t
 
+echo "Starting Ollama server..."
 ollama serve &
-ollama list
-ollama pull nomic-embed-text
+
+
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+  sleep 1
+done
+
+
+ollama pull llama3.2
