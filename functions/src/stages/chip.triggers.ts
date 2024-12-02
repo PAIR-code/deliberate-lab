b772a26fc36b3ceb8029087d417e74e1f5e451d0
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2024-12-02 11:08:27 -0500

    Update chip trigger to set next turn if all participants have responded to current offer

--- functions/src/stages/chip.triggers.ts
+++ functions/src/stages/chip.triggers.ts
@@ -3,26 +3,28 @@ import {
   ParticipantProfile,
   StageConfig,
   StageKind,
+  StagePublicData
 } from '@deliberation-lab/utils';
 
 import { app } from '../app';
 import {
-  getChipParticipantIds
+  getChipParticipantIds,
+  updateChipCurrentTurn
 } from './chip.utils';
 
 
 /**
   * When chip negotiation public data is updated,
-  * clear transactions and increment round
-  * if all participants have submitted an offer.
+  * update current turn/round if all participants have responded to current
+  * offer
   */
-export const completeChipNegotiationRound = onDocumentUpdated(
+export const completeChipTurn = onDocumentUpdated(
   {
     document:
     'experiments/{experimentId}/cohorts/{cohortId}/publicStageData/{stageId}'
   },
   async (event) => {
-    const stageDoc = app.firestore()
+    const publicDoc = app.firestore()
       .collection('experiments')
       .doc(event.params.experimentId)
       .collection('cohorts')
@@ -30,31 +32,69 @@ export const completeChipNegotiationRound = onDocumentUpdated(
       .collection('publicStageData')
       .doc(event.params.stageId);
 
+    const stageDoc = app.firestore()
+      .collection('experiments')
+      .doc(event.params.experimentId)
+      .collection('stages')
+      .doc(event.params.stageId);
+
     await app.firestore().runTransaction(async (transaction) => {
-      const stage = (await stageDoc.get()).data() as StageConfig;
-      if (stage.kind !== StageKind.CHIP) {
-        return;
+      const publicStage = (await publicDoc.get()).data() as StagePublicData;
+      if (publicStage.kind !== StageKind.CHIP) {
+        return false;
       }
 
-      // Check all cohort participants for offer
-      // TODO: Update based on new experiment design
+      const stageConfig = (await stageDoc.get()).data() as ChipStageConfig;
+      const numRounds = stageConfig.numRounds;
+
+      // If no offer, no need to update
+      if (!publicStage.currentTurn) {
+        return false;
+      }
+
+      // Check all cohort participants for response to offer
       const participantIds = await getChipParticipantIds(
         event.params.experimentId,
         event.params.cohortId
       );
 
-      const round = stage.currentRound;
+      const currentRound = publicStage.currentRound;
+      const acceptedOffer: string[] = [];
       for (const participantId of participantIds) {
         if (
-          !stage.participantOfferMap[round] ||
-          !stage.participantOfferMap[round][participantId]
+          participantId !== publicStage.currentTurn.participantId &&
+          !(participantId in publicStage.currentTurn.responseMap)
         ) {
+          // If an active participant (not the current sender) has not
+          // responded. do not proceed
           return false;
+        } else if (
+          participantId !== publicStage.currentTurn.participantId &&
+          publicStage.currentTurn.responseMap[participantId]
+        ) {
+          // Track participants who accepted the current offer
+          acceptedOffer.push(participantId);
         }
       }
 
-      // TODO: If all participants have submitted an offer, clear transactions
-      // TODO: Increment current round
+      // If all (non-offer) participants have responded to the offer,
+      // execute chip transaction
+      const sender = publicStage.currentTurn.participantId;
+      const recipient = acceptedOffer.length > 0 ?
+        acceptedOffer[Math.floor(Math.random() * acceptedOffer.length)] : null;
+
+      // TODO: Run chip offer transaction, i.e., update sender and receipient
+      // chip amounts and write relevant logs
+      console.log('Chip transaction', sender, recipient);
+
+      // Then, update current turn
+      publicStage.currentTurn = null;
+      const newData = updateChipCurrentTurn(
+        publicStage, participantIds, numRounds
+      );
+      transaction.set(publicDoc, newData);
     }); // end transaction
+
+    return true;
   }
 )
