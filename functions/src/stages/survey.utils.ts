6a663aa67f670322799b2193be4a28c8292b96cb
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-02-07 17:41:08 +0200

    improve documentation

--- functions/src/stages/survey.utils.ts
+++ functions/src/stages/survey.utils.ts
@@ -2,11 +2,19 @@ import {
     SurveyStageConfig,
     ExperimenterData,
     ParticipantProfileExtended,
-    createQuestionPrompt,
+    createSurveyQuestionPrompt,
 } from "@deliberation-lab/utils";
 import { getAgentResponse } from '../agent.utils';
 
 
+/**
+ * Ask an LLM actor to fill out each field in a survey.
+ * @param experimentId The current experiment ID
+ * @param experimenterData The ExperimenterData object for communicating with the LLM agent
+ * @param participant Participant profile information (used for debug messages only)
+ * @param stage The current Survey stage configuration object, containing the survey questions
+ * @returns A list of answers for each of the survey questions
+ */
 export async function getAgentParticipantSurveyResponse(
     experimentId: string,
     experimenterData: ExperimenterData, // for making LLM call
@@ -22,7 +30,7 @@ export async function getAgentParticipantSurveyResponse(
     const answers = [];
     // Build prompt
     for (let question of stage.questions) {
-        const prompt = createQuestionPrompt(question);
+        const prompt = createSurveyQuestionPrompt(question);
         // Call LLM API
         const response = await getAgentResponse(experimenterData, prompt);
         answers.push(response);
