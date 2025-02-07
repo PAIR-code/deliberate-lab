4a41d4f50df2f8337815aa0536de12467cba69f5
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-02-07 17:27:50 +0200

    make the debug messages easier to read

--- functions/src/stages/survey.utils.ts
+++ functions/src/stages/survey.utils.ts
@@ -7,31 +7,28 @@ import {
 import { getAgentResponse } from '../agent.utils';
 
 
-//TODO: add participant information to prompt
 export async function getAgentParticipantSurveyResponse(
     experimentId: string,
     experimenterData: ExperimenterData, // for making LLM call
     participant: ParticipantProfileExtended,
     stage: SurveyStageConfig) {
-    
-    const answers = []
-    // Build prompt
-    for (let question of stage.questions) {
-        const prompt = createQuestionPrompt(question);
-        console.debug("Prompt:", prompt)
-        // Call LLM API
-        const response = await getAgentResponse(experimenterData, prompt);
-        answers.push(response);
-    }
 
-    // Check console log for response
-    console.log(
+    console.debug(
         'TESTING AGENT PARTICIPANT PROMPT FOR RANKING STAGE\n',
         `Experiment: ${experimentId}\n`,
         `Participant: ${participant.publicId}\n`,
-        `Stage: ${stage.name} (${stage.kind})\n`,
-        answers
-    );
+        `Stage: ${stage.name} (${stage.kind})\n`);
+
+    const answers = [];
+    // Build prompt
+    for (let question of stage.questions) {
+        const prompt = createQuestionPrompt(question);
+        // Call LLM API
+        const response = await getAgentResponse(experimenterData, prompt);
+        answers.push(response);
+        console.debug("Prompt:", prompt)
+        console.debug("Answer:", response)
+    }
 
     return answers;
 }
