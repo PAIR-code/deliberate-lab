2b5dd3433db9b9c968b3abbb13efbd20c3e9b13d
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2025-02-14 04:30:10 -0800

    Move ApiKeyType enum from utils/src/experimenter to utils/src/agent

--- utils/src/agent.ts
+++ utils/src/agent.ts
@@ -10,3 +10,11 @@ export interface AgentGenerationConfig {
   presencePenalty: number;
   customRequestBodyFields: CustomRequestBodyField[];
 }
+
+// Specifies which API to use for model calls
+// TODO: Rename enum (ApiType? LLMApiType?)
+export enum ApiKeyType {
+  GEMINI_API_KEY = 'GEMINI',
+  OPENAI_API_KEY = 'OPENAI',
+  OLLAMA_CUSTOM_URL = 'OLLAMA',
+}
