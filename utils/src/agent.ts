cf769c7dba189c66c7801ea2c7d623a6cff1f0db
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2025-02-14 06:00:09 -0800

    Add functions to create new agent mediator configs

--- utils/src/agent.ts
+++ utils/src/agent.ts
@@ -1,7 +1,11 @@
+import {generateId} from './shared';
 import {StageKind} from './stages/stage';
 
-/** Agent types. */
+/** Agent types and functions. */
 
+// ************************************************************************* //
+// TYPES                                                                     //
+// ************************************************************************* //
 export interface CustomRequestBodyField {
   name: string;
   value: string;
@@ -51,8 +55,8 @@ export interface AgentMediatorModelSettings extends AgentModelSettings {
   // Whether the agent can respond multiple times in a row
   // (i.e., whether an agent's own message can trigger a new model call)
   canSelfTriggerCalls: boolean;
-  // Maximum total responses during the chat conversation
-  maxTotalResponses: number;
+  // Maximum total responses during the chat conversation (or null if no max)
+  maxTotalResponses: number | null;
 }
 
 // Specifies how prompt should be built
@@ -89,4 +93,70 @@ export interface AgentMediatorConfig {
   avatar: string;
   modelSettings: AgentMediatorModelSettings;
   promptSettings: AgentChatPromptConfig;
+}
+
+// ************************************************************************* //
+// CONSTANTS                                                                 //
+// ************************************************************************* //
+export const DEFAULT_AGENT_API_TYPE = ApiKeyType.GEMINI_API_KEY;
+
+export const DEFAULT_AGENT_API_MODEL = 'gemini-1.5-pro-latest';
+
+// ************************************************************************* //
+// FUNCTIONS                                                                 //
+// ************************************************************************* //
+
+export function createAgentModelSettings(
+  config: Partial<AgentModelSettings> = {},
+): AgentModelSettings {
+  return {
+    // TODO: pick first API that has a valid key?
+    apiType: config.apiType ?? DEFAULT_AGENT_API_TYPE,
+    // TODO: pick model name that matches API?
+    modelName: config.modelName ?? DEFAULT_AGENT_API_MODEL,
+    numRetries: config.numRetries ?? 0,
+    maxTokens: config.maxTokens ?? 8192,
+    temperature: config.temperature ?? 0.7,
+    topP: config.topP ?? 1.0,
+    frequencyPenalty: config.frequencyPenalty ?? 0.0,
+    presencePenalty: config.presencePenalty ?? 0.0,
+    customRequestBodyFields: config.customRequestBodyFields ?? [],
+  };
+}
+
+export function createAgentMediatorModelSettings(
+  config: Partial<AgentMediatorModelSettings> = {},
+): AgentMediatorModelSettings {
+  return {
+    ...createAgentModelSettings(config),
+    wordsPerMinute: config.wordsPerMinute ?? 80,
+    minMessagesBeforeResponding: config.minMessagesBeforeResponding ?? 0,
+    canSelfTriggerCalls: config.canSelfTriggerCalls ?? true,
+    maxTotalResponses: config.maxTotalResponses ?? null,
+  };
+}
+
+export function createAgentChatPromptConfig(
+  config: Partial<AgentChatPromptConfig> = {},
+): AgentChatPromptConfig {
+  return {
+    type: StageKind.CHAT,
+    prompt: config.prompt ?? '', // TODO: Set default chat prompt
+    includeStageHistory: config.includeStageHistory ?? false,
+    includeStageInfo: config.includeStageInfo ?? false,
+  };
+}
+
+export function createAgentMediatorConfig(
+  stageId: string, // ID of chat stage that agent should mediate
+  config: Partial<AgentMediatorConfig> = {},
+): AgentMediatorConfig {
+  return {
+    id: config.id ?? generateId(),
+    stageId,
+    name: config.name ?? 'Agent',
+    avatar: config.avatar ?? '🤖',
+    modelSettings: config.modelSettings ?? createAgentMediatorModelSettings(),
+    promptSettings: config.promptSettings ?? createAgentChatPromptConfig(),
+  };
 }
