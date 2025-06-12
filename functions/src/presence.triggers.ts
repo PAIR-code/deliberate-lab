c0490655318b339921461d4b3fc8c42ba9979cb6
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2025-06-12 08:51:53 -0700

    Temporarily hardcode all participants as connected
    This defines all new participants as connected and prevents existing
    participant profiles from switching to disconnected.

--- functions/src/presence.triggers.ts
+++ functions/src/presence.triggers.ts
@@ -11,30 +11,40 @@ import {app} from './app';
  */
 export const mirrorPresenceToFirestore = database
   .instance(`${process.env.GCLOUD_PROJECT}-default-rtdb`) // other parts of firebase use the -default-rtdb suffix, so stay consistent
-  .ref('/status/{experimentId}/{participantPrivateId}')  // rtdb path, not firestore path
+  .ref('/status/{experimentId}/{participantPrivateId}') // rtdb path, not firestore path
   .onWrite(async (change, context) => {
-    const { experimentId, participantPrivateId } = context.params;
-    console.log(`mirrorPresenceToFirestore triggered for experimentId=${experimentId} participantPrivateId=${participantPrivateId}`);
+    const {experimentId, participantPrivateId} = context.params;
+    console.log(
+      `mirrorPresenceToFirestore triggered for experimentId=${experimentId} participantPrivateId=${participantPrivateId}`,
+    );
     const status = change.after.val();
 
     if (!status) return null; // status was deleted
 
     // Find the matching participant doc
-    const participantRef = app.firestore().doc(`experiments/${experimentId}/participants/${participantPrivateId}`);
+    const participantRef = app
+      .firestore()
+      .doc(`experiments/${experimentId}/participants/${participantPrivateId}`);
     const participantSnapshot = await participantRef.get();
 
-    if (! participantSnapshot.exists) {
-      console.warn(`No participant found with id=${participantPrivateId} in experiment=${experimentId}`);
+    if (!participantSnapshot.exists) {
+      console.warn(
+        `No participant found with id=${participantPrivateId} in experiment=${experimentId}`,
+      );
       return null;
     }
     const participant = participantSnapshot.data();
 
-    if(participant && participant.agentConfig) {
-      return null;  // Don't update presence for agent participants
+    if (participant && participant.agentConfig) {
+      return null; // Don't update presence for agent participants
     }
 
-    return participantRef.set(
-      { connected: status.connected },
-      { merge: true }
-    );
+    // Temporarily prevent participants from switching from connected
+    // to disconnected (PR #537)
+    // TODO: Remove this logic to resume actual presence detection
+    if (!status.connected) {
+      return null;
+    }
+
+    return participantRef.set({connected: status.connected}, {merge: true});
   });
