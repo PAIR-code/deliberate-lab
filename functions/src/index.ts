0676302e8d0d720e2b18026563602f89fb22625f
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2025-06-19 18:06:15 -0700

    Consolidate all chat message related triggers into onChatMessageCreated trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,50 +1,28 @@
 /**
- * Register all functions.
+ * Register all Firebase Cloud functions.
  * All cloud functions are defined in their own files and imported here.
  */
 
-// Cloud functions
-export * from './experiment.endpoints';
-export * from './cohort.endpoints';
-
-export * from './participant.endpoints';
-export * from './participant.triggers';
-export * from './participant.utils';
-
-export * from './presence.triggers';
-
+// Endpoints called from frontend
 export * from './alert.endpoints';
-
 export * from './agent.endpoints';
-export * from './agent.utils';
-
-export * from './agent_participant.triggers';
-export * from './agent_participant.utils';
-
-export * from './log.utils';
-
+export * from './cohort.endpoints';
+export * from './experiment.endpoints';
 export * from './mediator.endpoints';
+export * from './participant.endpoints';
 
 export * from './stages/chat.endpoints';
-export * from './stages/chat.triggers';
-export * from './stages/chat.utils';
-
 export * from './stages/chip.endpoints';
-export * from './stages/chip.triggers';
-export * from './stages/chip.utils';
-
-export * from './stages/profile.utils';
 
 export * from './stages/ranking.endpoints';
-
 export * from './stages/salesperson.endpoints';
-export * from './stages/salesperson.utils';
-export * from './stages/salesperson.triggers';
-
-export * from './stages/stage.triggers';
-
 export * from './stages/survey.endpoints';
 
-// API functions
-export * from './api/gemini.api';
-export * from './api/openai.api';
+// Trigger functions
+export * from './agent_participant.triggers';
+export * from './participant.triggers';
+export * from './presence.triggers';
+
+export * from './stages/chat.triggers';
+export * from './stages/chip.triggers';
+export * from './stages/stage.triggers';
--- functions/src/stages/chat.triggers.ts
+++ functions/src/stages/chat.triggers.ts
@@ -1,16 +1,16 @@
 import {onDocumentCreated} from 'firebase-functions/v2/firestore';
 import {StageKind} from '@deliberation-lab/utils';
 import {
+  getFirestoreStage,
+  getFirestoreStagePublicData,
+} from '../utils/firestore';
+import {
   checkAgentsReadyToEndChat,
   sendAgentMediatorMessage,
   sendAgentParticipantMessage,
 } from './chat.agent';
-import {getChatStage, getChatStagePublicData} from './chat.utils';
-import {
-  getFirestoreStage,
-  getFirestoreStagePublicData,
-} from '../utils/firestore';
 import {startTimeElapsed} from './chat.time';
+import {sendAgentParticipantSalespersonMessage} from './salesperson.agent';
 
 // ************************************************************************* //
 // TRIGGER FUNCTIONS                                                         //
@@ -20,121 +20,59 @@ import {startTimeElapsed} from './chat.time';
 export const onChatMessageCreated = onDocumentCreated(
   'experiments/{experimentId}/cohorts/{cohortId}/publicStageData/{stageId}/chats/{chatId}',
   async (event) => {
-    // TODO: Add agent chat response logic here
-
-    const stage = await getChatStage(
+    const stage = await getFirestoreStage(
       event.params.experimentId,
       event.params.stageId,
     );
     if (!stage) return;
 
-    const publicStageData = await getChatStagePublicData(
+    const publicStageData = await getFirestoreStagePublicData(
       event.params.experimentId,
       event.params.cohortId,
       event.params.stageId,
     );
     if (!publicStageData) return;
 
-    // Start tracking elapsed time
-    startTimeElapsed(
-      event.params.experimentId,
-      event.params.cohortId,
-      publicStageData,
-    );
-  },
-);
-
-/** When chat message is created, generate mediator agent response if relevant. */
-export const createAgentMessage = onDocumentCreated(
-  {
-    document:
-      'experiments/{experimentId}/cohorts/{cohortId}/publicStageData/{stageId}/chats/{chatId}',
-    timeoutSeconds: 60, // Maximum timeout of 1 minute for typing delay.
-  },
-  async (event) => {
-    const data = event.data?.data() as ChatMessage | undefined;
-    const experimentId = event.params.experimentId;
-    const cohortId = event.params.cohortId;
-    const stageId = event.params.stageId;
-
-    // Use experiment config to get ChatStageConfig with agents.
-    const stage = await getChatStage(experimentId, stageId);
-    if (!stage) {
-      return;
+    // Take action for specific stages
+    switch (stage.kind) {
+      case StageKind.CHAT:
+        // Start tracking elapsed time
+        startTimeElapsed(
+          event.params.experimentId,
+          event.params.cohortId,
+          publicStageData,
+        );
+        break;
+      case StageKind.SALESPERSON:
+        sendAgentParticipantSalespersonMessage(
+          event.params.experimentId,
+          event.params.cohortId,
+          event.params.stageId,
+          stage,
+          event.params.chatId,
+        );
+        return; // Don't call any of the usual chat functions
+      default:
+        break;
     }
 
-    const publicStageData = await getChatStagePublicData(
-      experimentId,
-      cohortId,
-      stageId,
-    );
-
+    // Send agent mediator messages
     sendAgentMediatorMessage(
-      experimentId,
-      cohortId,
-      stage,
-      publicStageData,
-      event.params.chatId,
-    );
-  },
-);
-
-/** When chat message is created, generate agent participant response. */
-export const createAgentParticipantMessage = onDocumentCreated(
-  {
-    document:
-      'experiments/{experimentId}/cohorts/{cohortId}/publicStageData/{stageId}/chats/{chatId}',
-    timeoutSeconds: 60, // Maximum timeout of 1 minute for typing delay.
-  },
-  async (event) => {
-    const data = event.data?.data() as ChatMessage | undefined;
-    const experimentId = event.params.experimentId;
-    const stageId = event.params.stageId;
-    const cohortId = event.params.cohortId;
-
-    // Use experiment config to get ChatStageConfig with agents.
-    const stage = await getChatStage(experimentId, stageId);
-    if (!stage || stage.kind !== StageKind.CHAT) {
-      return;
-    }
-
-    const publicStageData = await getChatStagePublicData(
-      experimentId,
-      cohortId,
-      stageId,
-    );
-
-    sendAgentParticipantMessage(
-      experimentId,
-      cohortId,
-      stage,
-      publicStageData,
-      event.params.chatId,
-    );
-  },
-);
-
-/** When chat message is created, check if agent participants are
- * ready to end chat.
- */
-export const checkReadyToEndChat = onDocumentCreated(
-  {
-    document:
-      'experiments/{experimentId}/cohorts/{cohortId}/publicStageData/{stageId}/chats/{chatId}',
-    timeoutSeconds: 60, // Maximum timeout of 1 minute for typing delay.
-  },
-  async (event) => {
-    const data = event.data?.data() as ChatMessage | undefined;
-    const publicStageData = await getFirestoreStagePublicData(
       event.params.experimentId,
       event.params.cohortId,
-      event.params.stageId,
+      stage,
+      publicStageData,
+      event.params.chatId,
     );
-    const stage = await getFirestoreStage(
+    // Send agent participant messages
+    sendAgentParticipantMessage(
       event.params.experimentId,
-      event.params.stageId,
+      event.params.cohortId,
+      stage,
+      publicStageData,
+      event.params.chatId,
     );
-
+    // Check ready to end chat
     checkAgentsReadyToEndChat(
       event.params.experimentId,
       event.params.cohortId,
