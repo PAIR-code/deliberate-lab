8d7c843900a510c810a3b414027b867a98bcc1bb
Vivian Tsai <80003560+vivtsai@users.noreply.github.com>
2025-07-06 16:41:27 -0700

    Add timer, turn fields to PrivateChatStage
    This also sets similar group chat stage field for `requireFullTime` to
    just boolean rather than boolean|null (which was previously used for
    compatibility).

--- utils/src/stages/private_chat_stage.ts
+++ utils/src/stages/private_chat_stage.ts
@@ -27,6 +27,19 @@ import {
  */
 export interface PrivateChatStageConfig extends BaseStageConfig {
   kind: StageKind.PRIVATE_CHAT;
+  // If defined, ends chat after specified time limit
+  // (starting from when the first message is sent)
+  timeLimitInMinutes: number | null;
+  // Require participants to stay in chat until time limit is up
+  requireFullTime: boolean;
+  // If true, requires participant to go back and forth with mediator(s)
+  // (rather than being able to send multiple messages at once)
+  isTurnBasedChat: boolean;
+  // If turn based chat set to true, this specifies the max
+  // number of messages the participant can send (since the participant
+  // can only send one message per turn)
+  // If not set, there is no limit
+  maxNumberOfTurns: number | null;
 }
 
 // ************************************************************************* //
@@ -43,5 +56,9 @@ export function createPrivateChatStage(
     progress:
       config.progress ??
       createStageProgressConfig({waitForAllParticipants: true}),
+    timeLimitInMinutes: config.timeLimitInMinutes ?? null,
+    requireFullTime: config.requireFullTime ?? false,
+    isTurnBasedChat: config.isTurnBasedChat ?? true,
+    maxNumberOfTurns: config.maxNumberOfTurns ?? null,
   };
 }
