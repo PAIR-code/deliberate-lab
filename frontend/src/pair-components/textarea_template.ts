cf481dc88bfe11f685e38ce152d544b5b0ecba4a
rasmi <rasmi@google.com>
2025-12-03 17:46:46 -0500

    Make template validation contingent on alpha features being enabled.

--- frontend/src/pair-components/textarea_template.ts
+++ frontend/src/pair-components/textarea_template.ts
@@ -4,6 +4,8 @@ import {CSSResultGroup, html, nothing} from 'lit';
 import {customElement} from 'lit/decorators.js';
 
 import {core} from '../core/core';
+// TODO(rasmi): Remove AuthService import when variables are out of alpha
+import {AuthService} from '../services/auth.service';
 import {ExperimentEditor} from '../services/experiment.editor';
 
 import {
@@ -19,14 +21,23 @@ import {styles} from './textarea_template.css';
  * Extends pr-textarea and adds validation against experiment variables.
  *
  * Inherits all properties and behavior from pr-textarea automatically.
+ * Only performs validation when Alpha features are enabled.
  */
 @customElement('pr-textarea-template')
 export class TextAreaTemplate extends TextArea {
   static override styles: CSSResultGroup = [TextArea.styles, styles];
 
+  // TODO(rasmi): Remove authService when variables are out of alpha
+  private readonly authService = core.getService(AuthService);
   private readonly experimentEditor = core.getService(ExperimentEditor);
 
   override render() {
+    // TODO(rasmi): Remove this check when variables are out of alpha
+    // Only validate when Alpha features (including templating) are enabled
+    if (!this.authService.showAlphaFeatures) {
+      return super.render();
+    }
+
     const variableMap = extractVariablesFromVariableConfigs(
       this.experimentEditor.experiment?.variableConfigs ?? [],
     );
