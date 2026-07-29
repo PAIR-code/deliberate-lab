69c8e38c924a62179b7e2aa114de726bf0d2d972
Jacy Anthis <jacyanthis@gmail.com>
2026-07-29 13:25:34 -0700

    Revert "feat: API endpoint to reset a cohort in place"
    This reverts commit dd9bd62a5f4310b7ab87d11e4527de0cb2850fd2.

--- functions/src/dl_api/dl_api.endpoints.ts
+++ functions/src/dl_api/dl_api.endpoints.ts
@@ -30,7 +30,6 @@ import {
   getCohort,
   updateCohort,
   deleteCohort,
-  resetCohort,
 } from './cohorts.dl_api';
 import {listPersonas, uploadPersonas} from './personas.dl_api';
 
@@ -84,7 +83,6 @@ app.post('/v1/experiments/:experimentId/cohorts', createCohort);
 app.get('/v1/experiments/:experimentId/cohorts/:cohortId', getCohort);
 app.put('/v1/experiments/:experimentId/cohorts/:cohortId', updateCohort);
 app.delete('/v1/experiments/:experimentId/cohorts/:cohortId', deleteCohort);
-app.post('/v1/experiments/:experimentId/cohorts/:cohortId/reset', resetCohort);
 
 // API Routes - Persona banks
 app.get('/v1/experiments/:experimentId/personas', listPersonas);
