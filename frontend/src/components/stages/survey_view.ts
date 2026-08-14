319f8bab622c66c06bc9a50810cf1701fb5987d2
Crystal Qian <cjqian@users.noreply.github.com>
2026-08-14 11:28:50 -0400

    Allow scrolling in text fields

--- frontend/src/components/stages/survey_view.ts
+++ frontend/src/components/stages/survey_view.ts
@@ -247,7 +247,7 @@ export class SurveyView extends MobxLitElement {
           rows="5"
           placeholder="Type your response"
           .value=${textAnswer}
-          ?disabled=${this.participantService.disableStage}
+          ?readOnly=${this.participantService.disableStage}
           @input=${handleTextChange}
           .minLength=${minCount ?? nothing}
           .maxLength=${maxCount ?? nothing}
